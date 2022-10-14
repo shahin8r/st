5343d7f1a627f86dd0a12b260c52e8bfa135d68c
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2022-10-14 22:11:51 +0200

    Patch st-glyph-wide-support

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:size=9:antialias=true:hinting=true:lcdfilter=lcddefault:hintstyle=hintfull";
+static char *font = "Hack Nerd Font:size=10:antialias=true:hinting=true:lcdfilter=lcddefault:hintstyle=hintfull";
 static int borderpx = 8;
 
 /*
